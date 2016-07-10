8f47ab5247eba82ed017577fc55c0e5e72e1fc43
stur <sdongjie@126.com>
2016-07-10 21:56:14 +0800

    save

---
+++ src/test/java/com/coco/AppTest.java
@@ -0,0 +1,58 @@
+package com.coco;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        System.out.println("CC");
+
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+
+        WebDriverWait waiter = new WebDriverWait(driver, 5*1000);
+        WebElement eleItem=driver.findElements(By.id("lst-ib")).get(0);
+        eleItem.sendKeys("coco");
+
+        WebElement eleItem1=driver.findElements(By.name("btnK")).get(0);
+        eleItem1.click();
+      //  assertTrue( true );
+    }
+}
